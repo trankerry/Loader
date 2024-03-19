de5dd0e4f61d7f1a5a31e4f53dcc3caa2bfe0263
Kerry Tran <44415374+trankerry@users.noreply.github.com>
2024-03-18 22:27:44 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ input.addEventListener('input', function () {
 
 function Calc() {
 	const plates = [
-		{ value: [25, 'images/25kg.svg', 100], width: 100},
+		{ value: [25, 'images/25kg.svg', 100], width: 100 },
 		{ value: [20, 'images/20kg.svg', 100] },
 		{ value: [15, 'images/15kg.svg', 100] },
 		{ value: [10, 'images/10kg.svg', 100] },
@@ -26,23 +26,24 @@ function Calc() {
 
 	let sum = 0;
 	const platesNeeded = [];
-    const otherInfo = [];
+	const otherInfo = [];
 	let bar = document.getElementById('bar').value;
 	let weight = document.getElementById('weight').value;
 	let remainder = (weight - bar) / 2;
 	let noRed = document.getElementById('red25');
-    let convert = document.getElementById('convert');
+	let convert = document.getElementById('convert');
 
 	//if they don't want to use big red 25kg plates
 	if (!noRed.checked) {
 		plates.shift();
 	}
 
-    if(convert.checked) {
-        let lbsWeight = weight * 2.205;
+	if (convert.checked) {
+		let lbsWeight = weight * 2.205;
 		let rounded = lbsWeight.toFixed(2);
-		document.getElementById('lbs').innerHTML = 'You are lifting ' + rounded + ' lbs.';
-    }
+		document.getElementById('lbs').innerHTML =
+			'You are lifting ' + rounded + ' lbs.';
+	}
 
 	for (let i = 0; i < plates.length; i++) {
 		while (sum + plates[i].value[0] <= remainder) {
@@ -57,17 +58,22 @@ function Calc() {
 		"You'll need the following on each side:";
 	for (let j = 0; j < dups.length; j++) {
 		let li = document.createElement('li');
-        let img = document.createElement('img');
-        img.src = dups[j].value;
-        if (dups[j].value == 'images/25kg.svg' || dups[j].value == 'images/20kg.svg' || dups[j].value == 'images/15kg.svg' || dups[j].value == 'images/10kg.svg') {
-            img.width = 120;
-        }else {
-            img.width = 70;
-        }
-					
+		let img = document.createElement('img');
+		img.src = dups[j].value;
+		if (
+			dups[j].value == 'images/25kg.svg' ||
+			dups[j].value == 'images/20kg.svg' ||
+			dups[j].value == 'images/15kg.svg' ||
+			dups[j].value == 'images/10kg.svg'
+		) {
+			img.width = 120;
+		} else {
+			img.width = 70;
+		}
+
 		let text = document.createTextNode(dups[j].count + ' x ');
 		li.appendChild(text);
-        li.appendChild(img);
+		li.appendChild(img);
 		document.getElementById('answer').appendChild(li);
 	}
 }
